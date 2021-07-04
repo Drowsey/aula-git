1e6b5c3ae50f8ce2294a6848108379a0a4555e0d
Allen Cardoso <allenkevin.cardoso@gmail.com>
2021-07-04 20:03:39 -0300

    Adicionada a mensagem Como vai voce

--- Projeto-teste/Program.cs
+++ Projeto-teste/Program.cs
@@ -8,6 +8,7 @@ namespace Projeto_teste
         {
             Console.WriteLine("Olá Mundo");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como vai você?");
         }
     }
 }
